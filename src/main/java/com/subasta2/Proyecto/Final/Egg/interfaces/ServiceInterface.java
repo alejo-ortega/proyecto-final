685225ba254d8ee3c70ca1116187167e6286856e
AgusNico-java <agusnicorocha1998@gmail.com>
2022-04-02 12:38:00 -0300

    Metodos agregados a ServiceInterface.AuctionService conectada con AuctionRepository

--- src/main/java/com/subasta2/Proyecto/Final/Egg/interfaces/ServiceInterface.java
+++ src/main/java/com/subasta2/Proyecto/Final/Egg/interfaces/ServiceInterface.java
@@ -6,18 +6,49 @@ import javax.transaction.Transactional;
 /**
  * Interface that helps in the creation of important methods in the chosen services.
  * @author Nico
- * @param <T>
+ * @param <Object>
  */
-public interface ServiceInterface <T>{
+public interface ServiceInterface <Object>{
     
+    /**
+     * This method persists the changes into the Data Base.
+     * Can be used within update or create methods.
+     * @param object
+     * @throws Exception 
+     */
     @Transactional(rollbackOn = {Exception.class})
-    public void save (T object) throws Exception;
+    public void save (Object T) throws Exception;
     
-    public void validate (T object) throws Exception;
+    /**
+     * This method is used to validate data entered by the user. It should help preventing
+     * exceptions.
+     * @param object
+     * @throws Exception 
+     */
+    public void validate (Object T) throws Exception;
     
+    /**
+     * Shows a list with all the objects of the selected type.
+     * @return 
+     */
     @Transactional
-    public List <T> showList();
+    public List <Object> showList();
     
+    /**
+     * Shows one object based on its id attribute.
+     * @param id
+     * @return 
+     */
     @Transactional
-    public T showOne ();
+    public Object showOne (String id);
+    
+    /**
+     * Sets object attribute "active" to false
+     */
+    public void deactivate ();
+    
+    /**
+     * Sets object attribute "active" to true
+     */
+    public void activate ();
 }
