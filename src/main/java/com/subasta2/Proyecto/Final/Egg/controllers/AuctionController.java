162042ac4ea0468e38ee5b2502dd8275370f37ae
AgusNico-java <agusnicorocha1998@gmail.com>
2022-05-02 17:58:36 -0300

    Commit de mis últimas modificaciones a aucition controller y form

--- src/main/java/com/subasta2/Proyecto/Final/Egg/controllers/AuctionController.java
+++ src/main/java/com/subasta2/Proyecto/Final/Egg/controllers/AuctionController.java
@@ -2,10 +2,12 @@
 package com.subasta2.Proyecto.Final.Egg.controllers;
 
 import com.subasta2.Proyecto.Final.Egg.entities.Auction;
+import com.subasta2.Proyecto.Final.Egg.entities.Objects;
 import com.subasta2.Proyecto.Final.Egg.enums.Category;
 import com.subasta2.Proyecto.Final.Egg.services.AuctionService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,9 +15,12 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 @Controller
 @RequestMapping("/auction")
+@PreAuthorize("hasAnyRole('ROLE_ADMIN')")
 public class AuctionController {
     
     private final ObjectController objectController;
@@ -43,8 +48,7 @@ public class AuctionController {
     }
     
     /**
-     * Shows a Form tha allows the seller to create a new auction.If the auction
- already exist, the method allows you to edit its values.
+     * Shows a Form tha allows the seller to create a new auction.
      * @param categoryModel
      * @param stateModel
      * @param model
@@ -54,33 +58,37 @@ public class AuctionController {
      * @return
      */
     @GetMapping("/form")
-    public String showForm(ModelMap categoryModel, ModelMap stateModel, ModelMap objectModel, ModelMap auctionModel, @RequestParam(required = false) String objectId, @RequestParam(required = false) String auctionId){
+    public String showForm(ModelMap categoryModel, ModelMap stateModel, ModelMap objectModel, ModelMap auctionModel){
         
         try {
             objectController.categoryList(categoryModel);
             objectController.stateList(stateModel);
-            objectModel.addAttribute("objects", new Object());
-            objectController.showForm(objectModel, objectId);
-            auctionModel.addAttribute("auction", new Auction());
+            objectModel.addAttribute("objects", new Objects());
+            
+            auctionModel.addAttribute("auctions", new Auction());
         } catch (Exception e) {
             e.getStackTrace();
         }
-        return "auction/auction-form";
+        return "auction/auction-form.html";
     }
     
     /**
      * This method persist the auction information into the DataBase
+     * @param object
      * @param auction
      * @param model
      * @return 
      */
     @PostMapping("/form")
-    public String processForm(@ModelAttribute Auction auction, ModelMap model){
+    public String processForm(@ModelAttribute Objects object, ModelMap model, @ModelAttribute Auction auction){
         try{
+            objectController.objectService.save(object);
+            auction.setObjects(object);
+            ServletRequestAttributes attr = (ServletRequestAttributes) RequestContextHolder.currentRequestAttributes();
             auctionService.save(auction);
         }catch(Exception e){
             model.addAttribute("error "+ e.getMessage());
-            return "redirect:";
+            return "redirect:/auction/form";
         }
         return "redirect:/auction";
     }
