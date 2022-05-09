d71916b39f580333ef5476d169dc637d0c8d5863
Alejo Ortega <alejo1898ortega@gmail.com>
2022-05-09 18:14:52 -0300

    cambios vista&controlador subasta & index.html

--- src/main/java/com/subasta2/Proyecto/Final/Egg/controllers/AuctionController.java
+++ src/main/java/com/subasta2/Proyecto/Final/Egg/controllers/AuctionController.java
@@ -1,8 +1,10 @@
 package com.subasta2.Proyecto.Final.Egg.controllers;
 
 import com.subasta2.Proyecto.Final.Egg.entities.Auction;
+import com.subasta2.Proyecto.Final.Egg.entities.Objects;
 import com.subasta2.Proyecto.Final.Egg.enums.Category;
 import com.subasta2.Proyecto.Final.Egg.services.AuctionService;
+import com.subasta2.Proyecto.Final.Egg.services.ObjectsService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -20,11 +22,13 @@ public class AuctionController {
 
     private final ObjectController objectController;
     private final AuctionService auctionService;
+    private final ObjectsService objectsService;
 
     @Autowired
-    public AuctionController(AuctionService auctionService, ObjectController objectController) {
+    public AuctionController(AuctionService auctionService, ObjectController objectController, ObjectsService objectsService) {
         this.auctionService = auctionService;
         this.objectController = objectController;
+        this.objectsService = objectsService;
     }
 
     /**
@@ -43,8 +47,8 @@ public class AuctionController {
     }
 
     /**
-     * Shows a Form tha allows the seller to create a new auction.If the auction
-     * already exist, the method allows you to edit its values.
+     * Shows a form that allows the seller to create a new auction. If the
+     * auction already exist, the method allows you to edit its values.
      *
      * @param categoryModel
      * @param stateModel
@@ -84,10 +88,10 @@ public class AuctionController {
     }
 
     @GetMapping("/{id}")
-    public String auctionDetail(ModelMap auctionModel, @PathVariable("id") String id) {
+    public String auctionDetail(ModelMap model, @PathVariable("id") String id) {
         try {
             Auction auction = auctionService.showOne(id);
-            auctionModel.addAttribute("auction", auction);
+            model.addAttribute("auction", auction);
         } catch (Exception exception) {
             // TODO! handle error
         }
